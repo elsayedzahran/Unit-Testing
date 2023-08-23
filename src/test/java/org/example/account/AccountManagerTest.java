bf047c42d9165cf3f7c3153d5cb37502e1773753
Elsayed Zahran <elsayedzahran789@gmail.com>
2023-08-23 16:51:12 +0300

    repalce default assertion with assertj

--- src/test/java/org/example/account/AccountManagerTest.java
+++ src/test/java/org/example/account/AccountManagerTest.java
@@ -2,7 +2,8 @@ package org.example.account;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class AccountManagerTest {
     private AccountManagerImpl accountManager = new AccountManagerImpl();
@@ -11,37 +12,39 @@ public class AccountManagerTest {
     void testCustomerWithSuffientBalance(){
         Customer customerWithSuffientBalance = new Customer(1000,true,true);
         String result = accountManager.withdraw(customerWithSuffientBalance,800);
-        assertEquals("success", result);
+        assertThat(result).isEqualTo("success");
+
     }
     @Test
     void testCustomerWithOutCreditAllowed(){
         Customer customerWithOutCreditAllowed = new Customer(800,false,true);
         String result = accountManager.withdraw(customerWithOutCreditAllowed,900);
-        assertEquals("insufficient account balance", result);
+        assertThat(result).isEqualTo("insufficient account balance");
     }
     @Test
     void testCustomerWithExpectedBalanceMoreTHanCreditAndNotVip(){
         Customer customerWithExpectedBalanceMoreThanCreditAndNotVip = new Customer(100,true,false);
         String result = accountManager.withdraw(customerWithExpectedBalanceMoreThanCreditAndNotVip,1900);
-        assertEquals("maximum credit exceeded", result);
+        assertThat(result).isEqualTo("maximum credit exceeded");
+
     }
     @Test
     void testCustomerWithExpectedBalanceMoreTHanCreditAndVip(){
         Customer customerWithExpectedBalanceMoreThanCreditAndVip = new Customer(100,true,true);
         String result = accountManager.withdraw(customerWithExpectedBalanceMoreThanCreditAndVip,1900);
-        assertEquals("success", result);
+        assertThat(result).isEqualTo("success");
     }
     @Test
     void testCustomerWithExpectedBalanceLessTHanCreditAndNotVip(){
         Customer customerWithExpectedBalanceLessThanCreditAndNotVip = new Customer(1000,true,false);
         String result = accountManager.withdraw(customerWithExpectedBalanceLessThanCreditAndNotVip,1900);
-        assertEquals("success", result);
+        assertThat(result).isEqualTo("success");
     }
     @Test
     void testCustomerWithExpectedBalanceLessTHanCreditAndVip(){
         Customer customerWithExpectedBalanceLessThanCreditAndVip = new Customer(1000,true,true);
         String result = accountManager.withdraw(customerWithExpectedBalanceLessThanCreditAndVip,1900);
-        assertEquals("success", result);
+        assertThat(result).isEqualTo("success");
     }
 
 }
